8f191cf946d8ab5236f0bbb1ff4b6871a988c602
jcap49 <jcapecelatro@gmail.com>
2013-02-19 16:16:46 -0800

    reworked story element file

---
+++ storyelement.rb
@@ -0,0 +1,39 @@
+class StoryElements
+
+	def welcome_message
+		puts "[Insert creepy Goosebumps soundtrack]"
+		puts "Ooooo-ahhhh-oooo"
+		puts "This is the Goosebumps RPG: Say Cheese or Die."
+		puts "You have an option of one of three characters:"
+		
+		puts ""
+		puts ""
+		
+		puts "Good Guy Greg is unsurprisingly known as the good guy of the group."
+		puts "He is generally a nice human being, is a casual badminton player, and likes yams."
+		puts "His secret special power is the ability to do good."
+		
+		puts ""
+		puts ""
+
+		puts "Mickey The Stick, on the other hand, is a character of dubious nature."
+		puts "His name is apparently an homage to some baseball player he's got a signed bat from."
+		puts "But some kiddies on the block say it's in reference to the bat and something much more sinister..."
+		puts "His secret special power is the ability to swing his big bad bat."
+
+		puts "" 
+		puts ""
+
+		puts "Joey Bagga Donuts is that one kid who is just a bit off. You know that one kid."
+		puts "He's generally a light-hearted chump, but adds very little value to the group."
+		puts "He has this nervous habit of eating food, which most often comes in the form of donuts."
+		puts "Hence the nickname of course. And his special power is unsurprisingly..."
+		puts "...the ability to eat donuts which result in some soothing or healing shit."
+
+		puts ""
+		puts ""
+	end
+
+
+
+end
